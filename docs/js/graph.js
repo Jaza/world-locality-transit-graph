fb900daf1696432ebf5a0258506156ca4f3331a6
Jeremy Epstein <jazepstein@gmail.com>
2026-06-11 22:45:33 +1000

    move regions to a csv file

--- docs/js/graph.js
+++ docs/js/graph.js
@@ -34,255 +34,12 @@ ready(() => {
   const CSV_URL_PREFIX = (
     "https://raw.githubusercontent.com/Jaza/world-locality-transit-graph/master/csv/"
   );
+  const REGIONS_CSV_FILENAME = "regions.csv";
 
   const MAX_TRANSIT_TIME_MINS = 330;
 
-  const GRAPH_CODES = [
-    "au-arnhem-land",
-    "au-barkly",
-    "au-cape-york",
-    "au-east",
-    "au-east-kimberley",
-    "au-north",
-    "au-nsw-far-west",
-    "au-pilbara",
-    "au-qld-north-west",
-    "au-red-centre",
-    "au-sa-far-north",
-    "au-tas",
-    "au-west",
-    "au-west-kimberley",
-    "cl-norte-de-aysen",
-    "cl-sur-de-aysen",
-    "emea",
-    "jp-okinawa",
-    "north-america",
-    "nz-north-island",
-    "nz-south-island",
-    "patagonia-extremo-sur",
-    "south-america",
-    "tierra-del-fuego"
-  ];
-
-  const GRAPH_INFO_MAP = {
-    "au-arnhem-land": {
-      name: "AU Arnhem Land",
-      nodesCsvFilename: "au_arnhem_land_localities.csv",
-      nearbyEdgesCsvFilename: "au_arnhem_land_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_arnhem_land_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_arnhem_land_bounding_polygon.csv",
-      defaultCoords: {lat: -13.769, lng: 135.319},
-      defaultZoom: 7
-    },
-    "au-barkly": {
-      name: "AU Barkly",
-      nodesCsvFilename: "au_barkly_localities.csv",
-      nearbyEdgesCsvFilename: "au_barkly_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_barkly_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_barkly_bounding_polygon.csv",
-      defaultCoords: {lat: -19.856, lng: 134.398},
-      defaultZoom: 7
-    },
-    "au-cape-york": {
-      name: "AU Cape York",
-      nodesCsvFilename: "au_cape_york_localities.csv",
-      nearbyEdgesCsvFilename: "au_cape_york_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_cape_york_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_cape_york_bounding_polygon.csv",
-      defaultCoords: {lat: -12.631, lng: 142.690},
-      defaultZoom: 7
-    },
-    "au-east": {
-      name: "AU East",
-      nodesCsvFilename: "au_east_localities.csv",
-      nearbyEdgesCsvFilename: "au_east_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_east_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_east_bounding_polygon.csv",
-      defaultCoords: {lat: -28.009906, lng: 145.4592851},
-      defaultZoom: 5
-    },
-    "au-east-kimberley": {
-      name: "AU East Kimberley",
-      nodesCsvFilename: "au_east_kimberley_localities.csv",
-      nearbyEdgesCsvFilename: "au_east_kimberley_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_east_kimberley_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_east_kimberley_bounding_polygon.csv",
-      defaultCoords: {lat: -16.357, lng: 128.079},
-      defaultZoom: 7
-    },
-    "au-north": {
-      name: "AU North",
-      nodesCsvFilename: "au_north_localities.csv",
-      nearbyEdgesCsvFilename: "au_north_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_north_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_north_bounding_polygon.csv",
-      defaultCoords: {lat: -14.1, lng: 131.1},
-      defaultZoom: 7
-    },
-    "au-nsw-far-west": {
-      name: "AU NSW Far West",
-      nodesCsvFilename: "au_nsw_far_west_localities.csv",
-      nearbyEdgesCsvFilename: "au_nsw_far_west_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_nsw_far_west_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_nsw_far_west_bounding_polygon.csv",
-      defaultCoords: {lat: -31.51, lng: 143.53},
-      defaultZoom: 7
-    },
-    "au-pilbara": {
-      name: "AU Pilbara",
-      nodesCsvFilename: "au_pilbara_localities.csv",
-      nearbyEdgesCsvFilename: "au_pilbara_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_pilbara_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_pilbara_bounding_polygon.csv",
-      defaultCoords: {lat: -21.782, lng: 118.043},
-      defaultZoom: 7
-    },
-    "au-qld-north-west": {
-      name: "AU Qld North west",
-      nodesCsvFilename: "au_qld_north_west_localities.csv",
-      nearbyEdgesCsvFilename: "au_qld_north_west_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_qld_north_west_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_qld_north_west_bounding_polygon.csv",
-      defaultCoords: {lat: -20.710, lng: 140.449},
-      defaultZoom: 7
-    },
-    "au-red-centre": {
-      name: "AU Red Centre",
-      nodesCsvFilename: "au_red_centre_localities.csv",
-      nearbyEdgesCsvFilename: "au_red_centre_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_red_centre_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_red_centre_bounding_polygon.csv",
-      defaultCoords: {lat: -24.031, lng: 133.187},
-      defaultZoom: 7
-    },
-    "au-sa-far-north": {
-      name: "AU SA Far North",
-      nodesCsvFilename: "au_sa_far_north_localities.csv",
-      nearbyEdgesCsvFilename: "au_sa_far_north_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_sa_far_north_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_sa_far_north_bounding_polygon.csv",
-      defaultCoords: {lat: -28.705, lng: 134.539},
-      defaultZoom: 7
-    },
-    "au-tas": {
-      name: "AU Tasmania",
-      nodesCsvFilename: "au_tas_localities.csv",
-      nearbyEdgesCsvFilename: "au_tas_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_tas_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_tas_bounding_polygon.csv",
-      defaultCoords: {lat: -42.208, lng: 146.492},
-      defaultZoom: 8
-    },
-    "au-west": {
-      name: "AU West",
-      nodesCsvFilename: "au_west_localities.csv",
-      nearbyEdgesCsvFilename: "au_west_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_west_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_west_bounding_polygon.csv",
-      defaultCoords: {lat: -28.176, lng: 118.081},
-      defaultZoom: 6
-    },
-    "au-west-kimberley": {
-      name: "AU West Kimberley",
-      nodesCsvFilename: "au_west_kimberley_localities.csv",
-      nearbyEdgesCsvFilename: "au_west_kimberley_localities_transit_times.csv",
-      farEdgesCsvFilename: "au_west_kimberley_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "au_west_kimberley_bounding_polygon.csv",
-      defaultCoords: {lat: -17.958, lng: 122.245},
-      defaultZoom: 7
-    },
-    "cl-norte-de-aysen": {
-      name: "CL Norte de Aysén",
-      nodesCsvFilename: "cl_norte_de_aysen_localities.csv",
-      nearbyEdgesCsvFilename: "cl_norte_de_aysen_localities_transit_times.csv",
-      farEdgesCsvFilename: "cl_norte_de_aysen_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "cl_norte_de_aysen_bounding_polygon.csv",
-      defaultCoords: {lat: -45.487, lng: -72.389},
-      defaultZoom: 8
-    },
-    "cl-sur-de-aysen": {
-      name: "CL Sur de Aysén",
-      nodesCsvFilename: "cl_sur_de_aysen_localities.csv",
-      nearbyEdgesCsvFilename: "cl_sur_de_aysen_localities_transit_times.csv",
-      farEdgesCsvFilename: "cl_sur_de_aysen_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "cl_sur_de_aysen_bounding_polygon.csv",
-      defaultCoords: {lat: -47.236, lng: -72.614},
-      defaultZoom: 8
-    },
-    "emea": {
-      name: "EMEA",
-      nodesCsvFilename: "emea_localities.csv",
-      nearbyEdgesCsvFilename: "emea_localities_transit_times.csv",
-      farEdgesCsvFilename: "emea_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "emea_bounding_polygon.csv",
-      defaultCoords: {lat: 48.546, lng: 15.688},
-      defaultZoom: 5
-    },
-    "jp-okinawa": {
-      name: "JP Okinawa",
-      nodesCsvFilename: "jp_okinawa_localities.csv",
-      nearbyEdgesCsvFilename: "jp_okinawa_localities_transit_times.csv",
-      farEdgesCsvFilename: "jp_okinawa_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "jp_okinawa_bounding_polygon.csv",
-      defaultCoords: {lat: 26.4724, lng: 127.9193},
-      defaultZoom: 10
-    },
-    "north-america": {
-      name: "North America",
-      nodesCsvFilename: "north_america_localities.csv",
-      nearbyEdgesCsvFilename: "north_america_localities_transit_times.csv",
-      farEdgesCsvFilename: "north_america_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "north_america_bounding_polygon.csv",
-      defaultCoords: {lat: 53.49, lng: -101.25},
-      defaultZoom: 3
-    },
-    "nz-north-island": {
-      name: "NZ North Island",
-      nodesCsvFilename: "nz_north_island_localities.csv",
-      nearbyEdgesCsvFilename: "nz_north_island_localities_transit_times.csv",
-      farEdgesCsvFilename: "nz_north_island_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "nz_north_island_bounding_polygon.csv",
-      defaultCoords: {lat: -38.720, lng: 175.847},
-      defaultZoom: 7
-    },
-    "nz-south-island": {
-      name: "NZ South Island",
-      nodesCsvFilename: "nz_south_island_localities.csv",
-      nearbyEdgesCsvFilename: "nz_south_island_localities_transit_times.csv",
-      farEdgesCsvFilename: "nz_south_island_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "nz_south_island_bounding_polygon.csv",
-      defaultCoords: {lat: -43.914, lng: 170.431},
-      defaultZoom: 7
-    },
-    "patagonia-extremo-sur": {
-      name: "Patagonia Extremo Sur",
-      nodesCsvFilename: "patagonia_extremo_sur_localities.csv",
-      nearbyEdgesCsvFilename: "patagonia_extremo_sur_localities_transit_times.csv",
-      farEdgesCsvFilename: "patagonia_extremo_sur_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "patagonia_extremo_sur_bounding_polygon.csv",
-      defaultCoords: {lat: -51.456, lng: -71.406},
-      defaultZoom: 7
-    },
-    "south-america": {
-      name: "South America",
-      nodesCsvFilename: "south_america_localities.csv",
-      nearbyEdgesCsvFilename: "south_america_localities_transit_times.csv",
-      farEdgesCsvFilename: "south_america_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "south_america_bounding_polygon.csv",
-      defaultCoords: {lat: -26.71, lng: -62.93},
-      defaultZoom: 3
-    },
-    "tierra-del-fuego": {
-      name: "Tierra del Fuego",
-      nodesCsvFilename: "tierra_del_fuego_localities.csv",
-      nearbyEdgesCsvFilename: "tierra_del_fuego_localities_transit_times.csv",
-      farEdgesCsvFilename: "tierra_del_fuego_localities_transit_times_floyd_warshall_generated.csv",
-      boundingPolygonCsvFilename: "tierra_del_fuego_bounding_polygon.csv",
-      defaultCoords: {lat: -54.151, lng: -68.599},
-      defaultZoom: 7
-    }
-  };
-
+  let regionSlugs = null;
+  let regionInfoMap = null;
   let map = null;
   let nodes = null;
   let nodeLatsLons = null;
@@ -451,7 +208,7 @@ ready(() => {
 
   const loadBoundingPolygonFromUrl = (codes) => {
     Papa.parse(
-      `${getCsvUrlPrefix()}${GRAPH_INFO_MAP[codes[0]].boundingPolygonCsvFilename}`,
+      `${getCsvUrlPrefix()}${regionInfoMap[codes[0]].boundingPolygonCsvFilename}`,
       {
         download: true,
         header: true,
@@ -507,7 +264,7 @@ ready(() => {
 
   const loadFarEdgesFromUrl = (codes) => {
     Papa.parse(
-      `${getCsvUrlPrefix()}${GRAPH_INFO_MAP[codes[0]].farEdgesCsvFilename}`,
+      `${getCsvUrlPrefix()}${regionInfoMap[codes[0]].farEdgesCsvFilename}`,
       {
         download: true,
         header: true,
@@ -563,7 +320,7 @@ ready(() => {
 
   const loadNearbyEdgesFromUrl = (codes) => {
     Papa.parse(
-      `${getCsvUrlPrefix()}${GRAPH_INFO_MAP[codes[0]].nearbyEdgesCsvFilename}`,
+      `${getCsvUrlPrefix()}${regionInfoMap[codes[0]].nearbyEdgesCsvFilename}`,
       {
         download: true,
         header: true,
@@ -711,14 +468,14 @@ ready(() => {
     }
 
     nodes.addTo(map);
-    map.setView(GRAPH_INFO_MAP[codes[0]].defaultCoords, GRAPH_INFO_MAP[codes[0]].defaultZoom);
+    map.setView(regionInfoMap[codes[0]].defaultCoords, regionInfoMap[codes[0]].defaultZoom);
 
     loadNearbyEdgesFromUrl(codes);
   };
 
   const loadNodesFromUrl = (codes) => {
     Papa.parse(
-      `${getCsvUrlPrefix()}${GRAPH_INFO_MAP[codes[0]].nodesCsvFilename}`,
+      `${getCsvUrlPrefix()}${regionInfoMap[codes[0]].nodesCsvFilename}`,
       {
         download: true,
         header: true,
@@ -731,6 +488,81 @@ ready(() => {
           }
 
           loadNodesFromFile(codes, results.data);
+        }
+      }
+    );
+  };
+
+  const loadRegionsFromFile = (results) => {
+    if (!regionSlugs) {
+      regionSlugs = [];
+    }
+
+    if (!regionInfoMap) {
+      regionInfoMap = {};
+    }
+
+    const selectEl = document.createElement("select");
+    selectEl.setAttribute("multiple", "");
+    selectEl.id = GRAPH_SELECT_ID;
+
+    const defaultOptEl = document.createElement("option");
+    defaultOptEl.value = "";
+    defaultOptEl.text = "Choose region(s)...";
+    selectEl.appendChild(defaultOptEl);
+
+    while (results.length) {
+      const result = results.pop();
+
+      const slug = result.slug;
+      const slugUnderscored = slug.replaceAll("-", "_");
+      const name = result.name;
+
+      regionSlugs.push(slug);
+
+      regionInfoMap[slug] = {
+        name,
+        nodesCsvFilename: `${slugUnderscored}_localities.csv`,
+        nearbyEdgesCsvFilename: `${slugUnderscored}_localities_transit_times.csv`,
+        farEdgesCsvFilename: `${slugUnderscored}_localities_transit_times_floyd_warshall_generated.csv`,
+        boundingPolygonCsvFilename: `${slugUnderscored}_bounding_polygon.csv`,
+        defaultCoords: {lat: parseFloat(result.lat), lng: parseFloat(result.lon)},
+        defaultZoom: parseInt(result.zoom)
+      };
+    }
+
+    regionSlugs.sort();
+
+    regionSlugs.forEach((slug) => {
+      const optEl = document.createElement("option");
+      optEl.value = slug;
+      optEl.text = regionInfoMap[slug].name;
+      selectEl.appendChild(optEl);
+    });
+
+    selectEl.onchange = onGraphSelectChange;
+
+    const selectWrapperEl = document.createElement("div");
+    selectWrapperEl.appendChild(selectEl);
+
+    document.querySelector(GRAPH_SELECT_CONTAINER_NAME).appendChild(selectWrapperEl);
+  };
+
+  const loadRegionsFromUrl = () => {
+    Papa.parse(
+      `${getCsvUrlPrefix()}${REGIONS_CSV_FILENAME}`,
+      {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: 'greedy',
+        complete: (results) => {
+          if (!results.data.length) {
+            console.log('Warning: regions file is empty, not loading regions');
+            return;
+          }
+
+          loadRegionsFromFile(results.data);
         }
       }
     );
@@ -801,36 +633,11 @@ ready(() => {
     }
   };
 
-  const initGraphSelect = () => {
-    const selectEl = document.createElement("select");
-    selectEl.setAttribute("multiple", "");
-    selectEl.id = GRAPH_SELECT_ID;
-
-    const defaultOptEl = document.createElement("option");
-    defaultOptEl.value = "";
-    defaultOptEl.text = "Choose region(s)...";
-    selectEl.appendChild(defaultOptEl);
-
-    for (const code of GRAPH_CODES) {
-      const optEl = document.createElement("option");
-      optEl.value = code;
-      optEl.text = GRAPH_INFO_MAP[code].name;
-      selectEl.appendChild(optEl);
-    }
-
-    selectEl.onchange = onGraphSelectChange;
-
-    const selectWrapperEl = document.createElement("div");
-    selectWrapperEl.appendChild(selectEl);
-
-    document.querySelector(GRAPH_SELECT_CONTAINER_NAME).appendChild(selectWrapperEl);
-  };
-
   const init = () => {
     const containerEl = document.getElementById(CONTAINER_ID);
     if (containerEl) {
       initMap(CONTAINER_ID);
-      initGraphSelect();
+      loadRegionsFromUrl();
     }
   };
 
